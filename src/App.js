15a809e78c585a0474e6997f2ebdc94694dfb007
Stefano Pezzino <spezzino.13@gmail.com>
2016-10-11 22:17:42 -0300

    add header to the index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
+        <Header headerText="Flashcards" />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -21,12 +23,6 @@ class App extends Component {
 }
 
 const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
